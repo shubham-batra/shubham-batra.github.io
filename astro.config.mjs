6c70956488c1430b9cd6caecefa77d38c4883c49
shubham-batra <shubham.batra@outlook.com>
2025-11-13 17:42:49 -0800

    Setting up to deploy with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://shubham-batra.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
